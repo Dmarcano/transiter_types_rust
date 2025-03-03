8adcaf3f260c6d3150137e1823bf8107d6c9fd5f
Diego Marcano <marcanp.diego@gmail.com>
2025-03-03 00:51:54 -0500

    backwards compat for non 2024 edition projects

--- examples/transiter_types_examples/src/main.rs
+++ examples/transiter_types_examples/src/main.rs
@@ -1,11 +1,6 @@
 use rust_transiter_types::public_api_types::Stop;
 use serde_json;
 
-#[derive(serde::Deserialize, Debug, Clone)]
-struct ApiResponse {
-    stops: Vec<Stop>,
-}
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let response =
         reqwest::blocking::get("https://demo.transiter.dev/systems/us-ny-subway/stops/R09")?;
